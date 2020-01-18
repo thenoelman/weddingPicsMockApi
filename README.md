9eadeae878aa9ab521252adc0f11b33356bbbf07
Noel Yunginger <yungingn@oregonstate.edu>
2020-01-18 15:27:28 -0800

    more examples

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ instructions:
 
 1) Open a terminal (Mac) or command prompt (Windows)
 2) git clone https://github.com/thenoelman/weddingPicsMockApi.git
-3) npm install
-4) nodemon index.js
-5) navigate a browser (Chrome or Firefox preferred) to localhost:3000/
-6) I also recommend that you install the Moesif CORS plugin to Chrome or Firefox on your machine (and turn it on).  This will prevent your computer from thinking a cross-site scripting attack is occurring when you make http calls from React to your locally running mock API.
+3) cd weddingPicsMockApi
+4) npm install
+5) nodemon index.js
+6) navigate a browser (Chrome or Firefox preferred) to localhost:3000/
+7) I also recommend that you install the Moesif CORS plugin to Chrome or Firefox on your machine (and turn it on).  This will prevent your computer from thinking a cross-site scripting attack is occurring when you make http calls from React to your locally running mock API.
--- index.js
+++ index.js
@@ -1,14 +1,69 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
  
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
 })
 
 app.get('/whatever', (req, res) => {
-  res.send('whatever man');
+  res.send([1,2,3,]);
+})
+
+app.get('/whatever/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get('/test', (req, res) => {
+  var fs = require('fs');
+  fs.readFile( __dirname + '/response-files/test.json', function (err, data) {
+    if (err) {
+      throw err; 
+    }
+    console.log(data.toString());
+    res.send(data);
+  });
+  
+});
+
+app.get('/test2', (req, res) => {
+  res.send([1,2,3,]);
+  
+});
+
+app.get('/test3', (req, res) => {
+  testSTring = `[
+    {
+      "id": 1,
+      "title": "Nope",
+      "isDirty": true
+    },
+  {
+      "id": 2,
+      "title": "Yes",
+      "isDirty": false
+    }
+  ]`;
+  res.send(testSTring);
+})
+
+app.get('/test4', (req, res) => {
+  res.send([{"one": 1}]);
+})
+
+app.get('/test5', (req, res) => {
+  res.send([{"one": 1}, {"two": 2}]);
+})
+
+app.get('/test6', (req, res) => {
+  res.send({"thing": 88});
+})
+
+app.get('/test7', (req, res) => {
+  res.send({"thing": 88, "foo": 11, "bar": 3});
 })
  
-app.listen(3000, () => {
-    console.log('listening on port 3000...');
+//ASSIGN PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}...`);
 });
