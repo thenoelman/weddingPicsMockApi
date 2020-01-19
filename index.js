83e627d1575576b85fd9cb7ace3e08b0e009eb5d
Noel Yunginger <yungingn@oregonstate.edu>
2020-01-18 18:00:34 -0800

    more stuff

--- index.js
+++ index.js
@@ -25,13 +25,42 @@ app.get('/eventwall/:eventid', (req, res) => {
   
   if(req.params.eventid == "1")
   {
-    let rawdata = fs.readFileSync('./response-files/eventPhotos.json');
+    let rawdata = fs.readFileSync('./response-files/eventWall.json');
     let jsonData = JSON.parse(rawdata);
     res.send(jsonData);
   }
   else
   {
     res.send({"ERROR":"Invalid Event Id."});
+  }
+
+});
+
+//http://localhost:3333/getPhotoComments/<:photoid>
+//http://localhost:3333/getPhotoComments/1
+app.get('/getPhotoComments/:photoid', (req, res) => {
+  
+  if(req.params.photoid == "1")
+  {
+    let rawdata = fs.readFileSync('./response-files/photoComments_photo1.json');
+    let jsonData = JSON.parse(rawdata);
+    res.send(jsonData);
+  }
+  else if(req.params.photoid == "2")
+  {
+    let rawdata = fs.readFileSync('./response-files/photoComments_photo2.json');
+    let jsonData = JSON.parse(rawdata);
+    res.send(jsonData);
+  }
+  else if(req.params.photoid == "3")
+  {
+    let rawdata = fs.readFileSync('./response-files/photoComments_photo3.json');
+    let jsonData = JSON.parse(rawdata);
+    res.send(jsonData);
+  }
+  else
+  {
+    res.send({"ERROR":"Invalid Photo Id."});
   }
 
 });
